72165d22adedeb707f04daae03bbc1c05a3dd174
DanielDourado00 <danieljdpjorgedourado@gmail.com>
2023-10-03 17:45:22 -0300

    start game

--- TrabalhoEd1/lab.c
+++ TrabalhoEd1/lab.c
@@ -1,33 +1,48 @@
 #include "lab.h"
 
-/* lendo arquivo */
-
-void ReadMaze(const char *filename)
-{
-    FILE *file = fopen(filename, "r");              // abrindo o arquivo para leitura
-    if (file == NULL)                               // se o arquivo nao existir, entao saia do programa e mostre uma mensagem de erro
-    {
+/* Função para ler o labirinto do arquivo */
+void ReadMaze(const char *filename) {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
         printf("Erro ao abrir o arquivo\n");
         exit(1);
     }
 
-    int n;                                          // variavel para guardar o tamanho do labirinto
-    fscanf(file, "%d", &n);                         // lendo o primeiro elemento do arquivo que sera o tamanho do labirinto
-    printf("O tamanho do labirinto eh: %d\n", n);   // mostrando o tamanho do labirinto
+    int n;
+    fscanf(file, "%d", &n);
+    printf("O tamanho do labirinto eh: %d\n", n);
 
     int maze[n][n];
     int i, j;
-    for (i = 0; i < n; i++)
-    {
-        for (j = 0; j < n; ++j)
-        {
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; ++j) {
             fscanf(file, "%d", &maze[i][j]);
-            printf("%d ", maze[i][j]);
+            
+            /* Convertendo os valores em caracteres */
+            if (maze[i][j] == 0) {
+                maze[i][j] = ' ';  // Caminho livre
+
+
+            } else if (maze[i][j] == 1) {
+                maze[i][j] = '+';  // Parede
+                
+            } else if (maze[i][j] == 2) {
+                maze[i][j] = 'X';  // Posição do personagem
+            } else if (maze[i][j] == 3) {
+                maze[i][j] = 'O';  // Chegada
+            }
+        }
+    }
+
+    fclose(file);
+
+    /* Agora, você pode imprimir o labirinto com os caracteres corretos */
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            printf("%c ", maze[i][j]);
         }
         printf("\n");
     }
-    fclose(file);
 }
 
-/* printar labirinto */
 
--- TrabalhoEd1/lab.h
+++ TrabalhoEd1/lab.h
@@ -4,8 +4,28 @@
 para guardar o primeiro elemento do txt e logo depois, guardar os proximos valores.
 
 -> le os valores da matriz e salva na matriz.
+
+-> converter os valores da matriz em caracteres, para que eu possa imprimir bonitin
+
+-> tendo em vista que meus caminhos vazios sao ' ', devo andar com o personagem por eles até achar a saida
+(ultilizar backtracking)
+
+->criar uma funcao que percorra todo o espaço de estados possiveis (caminhos vazios) (BRUTAL FORCE)
+|_ funcao para verificar se uma posicao é possivel, ela te que ser diferente de 1 (parede)
+
+
+
+
+
+-> para isso devo salver as coordenadas dentro da minha PILHA
+
+
+->
 */
 
 #include "libs.h"
 
 void ReadMaze(const char *filename); // funcao para ler o meu arquivo labirinto.txt
+/* funcao para verificar se posicao x y é valida */
+bool PosicValida(int x, int y, int maze[n][n]);      // funcao para verificar se uma posicao é possivel, ela te que ser diferente de 1 (parede)
+bool encontrar(int x, int y, int maze[n][n]);          // funcao para verificar se a posicao é a chegada
